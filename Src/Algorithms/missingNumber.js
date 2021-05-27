68a16d1e76623f2248672aaa0d9a9cc13afe6ec8
dilhanamarasena <dilhan.amarasena@gmail.com>
2021-05-27 19:21:15 +0530

    added

--- Src/Algorithms/missingNumber.js
+++ Src/Algorithms/missingNumber.js
@@ -1,5 +1,5 @@
 let missing = [];
-let numArray = [32,36,30,31,37,38,39,33,34,40];
+let numArray = [32,36,30,31,37,38,39,33,40,35];
 const sortedNumArray = numArray.sort((a, b) => a - b);
 let mapdata=new Map();
 for (let index = 0; index < sortedNumArray.length-1; index++) {  
@@ -8,9 +8,33 @@ for (let index = 0; index < sortedNumArray.length-1; index++) {
     mapdata.set(sortedNumArray[index+1]+"-"+sortedNumArray[index],dif);
     
 }
-for (const element of mapdata) {
-    console.log(element);
-  }
+ var previousval=0;
+ largdataArr=[];
+ singleDataArr=[];
+  for (let [key, value] of  mapdata.entries()) {
+    if(previousval!=0){
+        if(previousval===value){
+            largdataArr.push(value);
+            previousval=value;
+        }else{
+            singleDataArr.push(value);
+        }
+       
+        console.log(key + " = " + value);
+    }else{
+        previousval=value;
 
+    } 
+}
 
+console.log(largdataArr);
+console.log(singleDataArr);
 
+let Keys = [...mapdata.entries()]
+        .filter(({ 1: v }) => v === singleDataArr[0])
+        .map(([k]) => k);
+
+console.log(Keys);
+keyArr=Keys.toString().split("-");
+var missingnumber=keyArr[0]-largdataArr[0];
+console.log("Missing number is ="+missingnumber);
